89449db225322e2095733fda00b0050afa838dfb
vihugarcia <vihugarcia@gmail.com>
2015-01-23 11:56:36 -0300

    Add helper classes

---
+++ common/models/RecordHelpers.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Victor
+ * Date: 23/01/2015
+ * Time: 11:50 AM
+ */
+
+namespace common\models;
+
+use yii;
+
+class RecordHelpers {
+
+    public static function userHas($model_name)
+    {
+        $connection = \Yii::$app->db;
+        $userid = Yii::$app->user->identity->id;
+        $sql = "SELECT id FROM $model_name WHERE user_id=:userid";
+        $command = $connection->createCommand($sql);
+        $command->bindValue(":userid", $userid);
+        $result = $command->queryOne();
+        if ($result == null) {
+            return false;
+        } else {
+            return $result['id'];
+        }
+    }
+    
+}
---
+++ common/models/ValueHelpers.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Victor
+ * Date: 23/01/2015
+ * Time: 11:35 AM
+ */
+
+namespace common\models;
+
+
+class ValueHelpers {
+
+    /**
+     * return the value of a role name handed in as string
+     * example: 'Admin'
+     *
+     * @param mixed $role_name
+     */
+    public static function getRoleValue($role_name)
+    {
+        $connection = \Yii::$app->db;
+        $sql = "SELECT role_value FROM role WHERE role_name=:role_name";
+        $command = $connection->createCommand($sql);
+        $command->bindValue(":role_name", $role_name);
+        $result = $command->queryOne();
+
+        return $result['role_value'];
+    }
+
+    /**
+     * return the value of a status name handed in as string
+     * example: 'Active'
+     * @param mixed $status_name
+     */
+    public static function getStatusValue($status_name)
+    {
+        $connection = \Yii::$app->db;
+        $sql = "SELECT status_value FROM status WHERE status_name=:status_name";
+        $command = $connection->createCommand($sql);
+        $command->bindValue(":status_name", $status_name);
+        $result = $command->queryOne();
+        return $result['status_value'];
+    }
+
+    /**
+     * returns value of user_type_name so that you can
+     * used in PermissionHelpers methods
+     * handed in as string, example: 'Paid'
+     *
+     * @param mixed $user_type_name
+     */
+    public static function getUserTypeValue($user_type_name)
+    {
+        $connection = \Yii::$app->db;
+        $sql = "SELECT user_type_value FROM user_type WHERE user_type_name=:user_type_name";
+        $command = $connection->createCommand($sql);
+        $command->bindValue(":user_type_name", $user_type_name);
+        $result = $command->queryOne();
+        return $result['user_type_value'];
+    }
+
+}
