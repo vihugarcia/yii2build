0bcc9c8a18163502aa608118426d61ece52549fd
vihugarcia <vihugarcia@gmail.com>
2015-01-27 17:50:08 -0300

    Add profile CRUD

--- frontend/models/Profile.php
+++ frontend/models/Profile.php
@@ -51,6 +51,16 @@ class Profile extends \yii\db\ActiveRecord
         ];
     }
 
+    public function beforeValidate()
+    {
+        if ($this->birthdate != null) {
+            $new_date_format = date('Y-m-d', strtotime($this->birthdate));
+            $this->birthdate = $new_date_format;
+        }
+
+        return parent::beforeValidate();
+    }
+
     /**
      * @inheritdoc
      */
